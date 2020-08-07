3e058af4e5bc61c9182c9417e6809bfb36d857e1
Vaibhav <varora@catalystone.com>
2020-08-07 16:42:47 +0530

    feat: 🎸 add banner with data to application

---
+++ src/Banner.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import requests from './requests';
+
+function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+
+            const randomIndex = Math.floor(Math.random() * request.data.results.length - 1);
+            setMovie(request.data.results[randomIndex]);
+            return request;
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <header className="banner" style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            backgroundPosition: "center center",
+        }}>
+            <div className="banner__contents">
+                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+                <h1 className="banner__description">
+                    {movie?.overview}
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Banner
