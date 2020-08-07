4df7154df267624b66e357e4ea2cae7795d0a980
Vaibhav <varora@catalystone.com>
2020-08-07 14:50:52 +0530

    fix: 🐛 correct wrong param name of api_key for NetflixOriginal

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const API_KEY = `04b1d87dd9766d99514d310bf8277cd0`;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_keys=${API_KEY}&with_networks=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
