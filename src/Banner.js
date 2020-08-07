811e220c6ee36932bb6f980a8af10c1b4448185e
Vaibhav <varora@catalystone.com>
2020-08-07 17:00:43 +0530

    style: 💄 make banner look much better and closer to original

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -16,6 +17,10 @@ function Banner() {
         fetchData();
     }, []);
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+
     return (
         <header className="banner" style={{
             backgroundSize: "cover",
@@ -23,13 +28,13 @@ function Banner() {
             backgroundPosition: "center center",
         }}>
             <div className="banner__contents">
-                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+                <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                    {movie?.overview}
+                    {truncate(movie?.overview, 150)}
                 </h1>
             </div>
         </header>
